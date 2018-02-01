dff1aacf0b6a67879452fc3f279baefe86c3fa93
bangquan.feng <bangquan.feng@tcl.com>
2018-02-01 20:19:56 +0800

    Add MainFragment

---
+++ app/src/main/java/com/fengbangquan/facephoto/Mainfragment.java
@@ -0,0 +1,98 @@
+package com.fengbangquan.facephoto;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fengbangquan.facephoto.adapter.MainAdapter;
+import com.fengbangquan.facephoto.data.MediaItem;
+import com.fengbangquan.facephoto.data.MediaLoader;
+import com.fengbangquan.facephoto.utils.faceutil.FaceDetectionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Feng Bangquan on 18-2-1
+ */
+public class Mainfragment extends Fragment implements LoaderManager.LoaderCallbacks<List<MediaItem>> {
+    
+    private MainAdapter mMainAdapter;
+    private RecyclerView mMainView;
+    private List<String> mImageList;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        mMainView = view.findViewById(R.id.recycler_view_main);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public Loader<List<MediaItem>> onCreateLoader(int id, Bundle args) {
+        return new MediaLoader(getContext(), MediaLoader.IMAGE_AND_VIDEO);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<MediaItem>> loader, List<MediaItem> data) {
+        mImageList = new ArrayList<>();
+        mMainView.setAdapter(new MainAdapter(getContext(), data));
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).duration() == 0) {
+                mImageList.add(data.get(i).filePath());
+            }
+        }
+        if (mImageList.size() > 0) {
+            Intent intent = new Intent(getActivity(), FaceDetectionService.class);
+            getActivity().startService(intent);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<MediaItem>> loader) {
+
+    }
+}
---
+++ app/src/main/java/com/fengbangquan/facephoto/adapter/MainAdapter.java
@@ -0,0 +1,45 @@
+package com.fengbangquan.facephoto.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fengbangquan.facephoto.data.MediaItem;
+
+import java.util.List;
+
+/**
+ * Created by Feng Bangquan on 18-2-1
+ */
+public class MainAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context mContext;
+    private List<MediaItem> mItemList;
+    public MainAdapter(Context context, List<MediaItem> list) {
+        mContext = context.getApplicationContext();
+        mItemList = list;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
